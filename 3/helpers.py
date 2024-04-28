d36dd8fe5880fb2810e81a14cec73d71d81cc62f
Dmitriy-U <dmitriyushkalov@gmail.com>
2024-04-28 15:02:25 +0300

    Created the checking has file

--- 3/helpers.py
+++ 3/helpers.py
@@ -1,49 +1,20 @@
-import math
 
 
-def get_prime_numbers(start, end) -> list[int]:
-    """
-    Найти натуральные числа в диапазоне
-    :param start: начало диапазона поиска
-    :param end: конец диапазона поиска
-    :return: найденные натуральные числа
-    """
+def has_handler(fs: dict[str,dict[str,bytes]], file: None | str) -> tuple[bool, str]:
+    assert file is not None, "Вы не указали аргумент --file"
 
-    result = []
+    has_file = file in fs
 
-    for number in range(start, end + 1):
-        if number < 0 or number == 0 or number == 1:
-            continue
+    if has_file:
+        message = f"Файл {file} есть"
+    else:
+        message = f"Файл {file} отстствует"
 
-        if all(number % i != 0 for i in range(2, int(number ** 0.5) + 1)):
-            result.append(number)
-
-    return result
+    return has_file, message
 
 
-def get_ranges_by_number_of_workers(number_of_workers: int, prime_range_start: int, prime_range_end: int) -> list[tuple[int]]:
-    """
-    Получить диапазоны в зависимости от количества рабочих
-    :param number_of_workers: количество рабочих
-    :param prime_range_start: начало диапазона поиска
-    :param prime_range_утв: конец диапазона поиска
-    :return: список с диапазонами поиска
-    """
-    
-    digit_quantity = prime_range_end - prime_range_start
+def read_handler(fs: dict[str,dict[str,bytes]], file: None | str) -> dict[str,bytes]:
+    assert file is not None, "Вы не указали аргумент --file"
+    assert file in fs, f"Файл {file} отсутствует"
 
-    digit_part = math.ceil(digit_quantity / number_of_workers)
-
-    tasks = []
-    
-    for worker_index in range(number_of_workers):
-        worker_prime_range_start = prime_range_start + worker_index * digit_part
-        if number_of_workers == worker_index + 1:
-            worker_prime_range_end = prime_range_end
-        else:
-            worker_prime_range_end = (prime_range_start + (worker_index + 1) * digit_part) - 1
-        
-        tasks.append((worker_prime_range_start, worker_prime_range_end,))
-    
-    return tasks
-        
+    return fs[file]
--- 3/main.py
+++ 3/main.py
@@ -3,9 +3,20 @@ import argparse
 
 from enum import Enum
 
+from helpers import has_handler, read_handler
+
+fs: dict[str, dict[str, bytes]] = {
+    "/test.txt": {
+        "2:1": b"0xff",
+        "2:2": b"0x00"
+    }
+}
 
 HOST = "127.0.0.255"
 PORT = 9999
+BYTE_BLOCK_LENGTH = 1024
+EXTERNAL_NODE_COUNT = 1
+
 
 class Command(Enum):
     RUN = 'run'
@@ -14,7 +25,7 @@ class Command(Enum):
     WRITE = 'write'
     DELETE = 'delete'
     CHANGE_BLOCK = 'changeblock'
-    
+
     @classmethod
     def list(cls) -> str:
         return list(map(lambda c: c.value, cls))
@@ -29,38 +40,91 @@ parser.add_argument('--block_data', '-BD', metavar="BLOB", help='данные б
 args = parser.parse_args()
 
 command = args.command
-file = args.file
-file_source = args.file_source
-block = args.block
-block_data = args.block_data
+file: None | str = args.file
+file_source: None | str = args.file_source
+block: None | int = args.block
+block_data: None | bytes = args.block_data
 
 if command == Command.RUN.value:
     print('Хранение запущено')
-    
+
     context = zmq.Context()
 
     receiver = context.socket(zmq.PULL)
-    receiver.bind("tcp://*:9000")
+    receiver.bind("tcp://127.0.0.255:9000")
+
+    sender = context.socket(zmq.PUSH)
+    sender.connect("tcp://127.0.0.255:9100")
+
+    while True:
+        request = receiver.recv().decode('utf8')
+        data = request.split(":")
+        command, file = data
+
+        match command:
+            case Command.HAS.value:
+                has_file, _ = has_handler(fs, file)
+                sender.send_string(f"{request}:{int(has_file)}")
+            case Command.READ.value:
+                print("READ")
+            case Command.WRITE.value:
+                print("WRITE")
+            case Command.DELETE.value:
+                print("DELETE")
+            case Command.CHANGE_BLOCK.value:
+                print("CHANGE_BLOCK")
+            case _:
+                print("Команда отсутствует")
+                exit(0)
+else:
+    context = zmq.Context()
+
+    receiver = context.socket(zmq.PULL)
+    receiver.bind("tcp://127.0.0.255:9100")
 
     sender = context.socket(zmq.PUSH)
     sender.connect("tcp://127.0.0.255:9000")
 
-    while True:
-        data = receiver.recv()
-        
-        print('Has result: ', data.decode('utf8'))
-        sender.send_string(data.decode('utf8'))
-        print('Sended -->')
-else:
+    result: str | None = None
+
     match command:
         case Command.HAS.value:
-            print("HAS")
+            try:
+                has_file, message = has_handler(fs, file)
+            except AssertionError as e:
+                print(e)
+                exit(0)
+
+            if has_file:
+                request = f"has:{file}"
+                sender.send_string(request)
+
+                has_external_file = False
+                i = 0
+                while not has_external_file and i < EXTERNAL_NODE_COUNT:
+                    response = receiver.recv().decode('utf8')
+                    # TODO
+                    print(response, request, response.find(request))
+                    if response.find(request):
+                        data = response.split(":")[-1:]
+
+                        print('Has result: ', data)
+                        print('Sended -->')
+                        i += 1
+
+            print(message)
         case Command.READ.value:
-            print("READ")
+            try:
+                _, message = read_handler(fs, file)
+                print(message)
+            except AssertionError as e:
+                print(e)
         case Command.WRITE.value:
             print("WRITE")
         case Command.DELETE.value:
             print("DELETE")
+        case Command.CHANGE_BLOCK.value:
+            print("CHANGE_BLOCK")
         case _:
             print("Команда отсутствует")
-            exit(1)
+            exit(0)
