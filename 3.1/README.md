b6064b39f099a014b4c07e9f2d03a15ce3a7dd84
Dmitriy-U <dmitriyushkalov@gmail.com>
2024-06-04 00:01:05 +0300

    Added Dockerfile

--- 3.1/README.md
+++ 3.1/README.md
@@ -2,10 +2,36 @@
 
 Централизованное хранение данных
 
-## Запуск хранения данных
+## Запуск хранения данных в *docker* контейнере
+
+### Создание образа
+
+Запустите: `docker buildx build --tag <image_name> .`, где:
+
+- *image_name* - наименование образа
+
+#### Windows
 
 ```ps
-fastapi run .\storage\main.py --host 127.0.0.1 --port 9000
+docker buildx build --tag laboratory-3 .
+```
+
+#### linux
+
+```shell
+docker build --tag laboratory-3 .
+```
+
+### Создание и запуск контейнера
+
+Запустите: `docker run --name <container_name> -p <port>:9000 -d <image_name>`, где:
+
+- *container_name* - наименование контейнера,
+- *port* - номер порта на котором будет работать хранилище
+- *image_name* - наименование образа
+
+```shell
+docker run --name laboratory-3-9000 -p 9000:9000 -d laboratory-3
 ```
 
 ## Команды
@@ -22,7 +48,7 @@ python.exe .\app.py -C read -F .\demo.txt
 python.exe .\app.py -C read -F .\demo.txt -O .\demo1.txt
 ```
 
-### Запись фала
+### Запись файла
 
 ```ps
 python.exe .\app.py -C write -F .\demo.txt -I .\demo.txt
---
+++ 3.1/app/main.py
@@ -0,0 +1,87 @@
+from app.models import Base
+from app.crud import db_delete_file, db_get_file_block, db_get_file_blocks, db_get_file_storages
+from app.constants import BYTE_BLOCK_LENGTH
+from app.database import SessionLocal, engine
+from app.helpers import (Command, get_config, get_file_data, parse_args_main, storage_change_file_block,
+                         storage_delete_file, write_file)
+
+Base.metadata.create_all(bind=engine)
+
+
+def main():
+    (command, file_path, input_file_data, output, block_id, block_data) = parse_args_main()
+
+    match command:
+        case Command.READ.value:
+            assert file_path is not None, "Вы не указали атрибут --file-path"
+
+            with SessionLocal() as session:
+                db_file_blocks = db_get_file_blocks(session, file_path)
+
+            assert len(db_file_blocks) > 0, f"Файл {file_path} отсутствует"
+
+            file_data = get_file_data(db_file_blocks)
+
+            if output is None:
+                print(file_data)
+            else:
+                with open(output, 'wb') as file:
+                    file.write(file_data)
+
+            exit(0)
+        case Command.WRITE.value:
+            assert file_path is not None, "Вы не указали атрибут --file-path"
+            assert input_file_data is not None, "Вы не указали атрибут --input"
+            config = get_config()
+            host_list: list[str] = config.get('hostList', None)
+
+            assert host_list is not None, "Вы не указали атрибут --host-list"
+
+            with SessionLocal() as session:
+                write_success = write_file(session, file_path, input_file_data, host_list)
+
+            if not write_success:
+                print(f"Файл {file_path} не был записан")
+                exit(1)
+                return
+
+            print(f"Файл {file_path} был записан")
+            exit(0)
+        case Command.DELETE.value:
+            assert file_path is not None, "Вы не указали атрибут --file-path"
+
+            with SessionLocal() as session:
+                db_file_blocks = db_get_file_storages(session, file_path)
+
+                storage_addresses: set[str] = set()
+                for block in db_file_blocks:
+                    storage_addresses.add(block[0])
+
+                is_deleted = storage_delete_file(file_path, storage_addresses)
+
+                if is_deleted:
+                    db_delete_file(session, file_path)
+
+            print(f"Файл {file_path} удалён" if is_deleted else f"Файл {file_path} не был удалён")
+            exit(0)
+        case Command.CHANGE_BLOCK.value:
+            assert file_path is not None, "Вы не указали атрибут --file-path"
+            assert block_id is not None, "Вы не указали атрибут --block-id"
+            assert block_data is not None, "Вы не указали атрибут --block-data"
+            assert len(block_data) == BYTE_BLOCK_LENGTH, f"Длина блока должна составлять {BYTE_BLOCK_LENGTH} символов"
+
+            with SessionLocal() as session:
+                db_file_block = db_get_file_block(session, file_path, str(block_id))
+
+            assert db_file_block is not None, f"Файл {file_path} отсутствует"
+
+            block_changed = storage_change_file_block(db_file_block, block_data)
+
+            if block_changed:
+                print(f"Блок {block_id} файла {file_path} изменён")
+            else:
+                print(f"Блок {block_id} файла {file_path} не изменён")
+
+
+if __name__ == "__main__":
+    main()
