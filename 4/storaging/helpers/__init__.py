aa933956819fd09c1c0ea944858331e55a4a3476
Dmitriy-U <dmitriyushkalov@gmail.com>
2024-06-10 00:36:58 +0300

    Inited 4-th laboratory

---
+++ 4/storaging/helpers/__init__.py
@@ -0,0 +1,15 @@
+import hashlib
+import socket
+
+
+def get_self_ip_address(remote_server="google.com") -> str:
+    """
+    Return the/a network-facing IP number for this system.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect((remote_server, 80))
+        return s.getsockname()[0]
+
+
+def get_hash(string: bytes) -> int:
+    return int(hashlib.sha1(string).hexdigest(), 16) % (10 ** 6)
---
+++ 4/storaging/main.py
@@ -0,0 +1,33 @@
+import socket
+import json
+
+from fastapi import FastAPI
+
+from .helpers import get_self_ip_address, get_hash
+
+UDP_PORT = 9000
+IP_ADDRESS = get_self_ip_address()
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.on_event("startup")
+async def startup_event():
+    ip_address = get_self_ip_address()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    advertise = {
+        "hash": get_hash(ip_address.encode()),
+        "key": "test"
+    }
+    sock.sendto(bytes(json.dumps(advertise), "utf-8"), ('127.0.0.2', UDP_PORT))
