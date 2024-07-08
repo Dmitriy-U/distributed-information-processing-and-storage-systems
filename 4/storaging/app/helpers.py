1f9148a7ebb1a9767a8b039f6f98dc8562b58a0a
Dmitriy Ushkalov <dmitriyushkalov@gmail.com>
2024-07-08 20:23:33 +0300

    Изменил хэш-функцию

--- 4/storaging/app/helpers.py
+++ 4/storaging/app/helpers.py
@@ -6,15 +6,23 @@ from humps import camel
 
 def get_self_ip_address(remote_server="google.com") -> str:
     """
-    Return the/a network-facing IP number for this system.
+    Получение собственного IP-адреса
+    remote_server: с помощью этого сервера мы узнаем свой адрес в ответе
     """
+
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect((remote_server, 80))
         return s.getsockname()[0]
 
 
-def get_hash(string: bytes) -> int:
-    return int(hashlib.sha1(string).hexdigest(), 16) % (10 ** 9)
+def get_hash(string: bytes, hash_bit=8) -> int:
+    """
+    Получение хеша (по-умолчанию 8-битного)
+
+    string: fsdf
+    """
+
+    return int(hashlib.sha1(string).hexdigest()[-int(hash_bit / 4):], 16)
 
 
 def to_camel_case(string):
