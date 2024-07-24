64ff023811cb99b7f9ab7e050a34c97c2d01f17e
Dmitriy-U <dmitriyushkalov@gmail.com>
2024-07-25 00:55:22 +0300

    Алгоритм поиска предыдущего и следующего хешей относительно целевого

--- 4/storaging/app/helpers.py
+++ 4/storaging/app/helpers.py
@@ -27,3 +27,37 @@ def get_hash(string: bytes, hash_bit=HASH_BIT_COUNT) -> int:
 
 def to_camel_case(string):
     return camel.case(string)
+
+
+def search_before_and_after_nodes(target_hash: int, node_hash_list: list[int]):
+    """
+    Поиск предыдущего и следующего хешей по модулю относительно целевого хеша среди списка хешей
+    :param target_hash: необходимый хеш
+    :param node_hash_list: список хешей
+    :return: предыдущий и следующий хеши относительно целевого
+    """
+
+    node_hash_list_len = len(node_hash_list)
+
+    if node_hash_list_len == 0:
+        return None, None
+
+    if node_hash_list_len == 1:
+        node_hash_single = node_hash_list[0]
+        return node_hash_single, node_hash_single
+
+    node_hash_before, node_hash_after = None, None
+    for index, node_hash in enumerate(node_hash_list):
+        if target_hash >= node_hash:
+            node_hash_before = node_hash
+        else:
+            node_hash_after = node_hash
+            break
+
+    if node_hash_before is None:
+        node_hash_before = node_hash_list[-1]
+
+    if node_hash_after is None:
+        node_hash_after = node_hash_list[1]
+
+    return node_hash_before, node_hash_after
