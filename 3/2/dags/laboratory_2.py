be5f33933751f8383a55a7819092c390dc03b246
Dmitriy-U <dmitriyushkalov@gmail.com>
2025-01-08 02:23:22 +0300

    Change dag-file

--- 3/2/dags/laboratory_2.py
+++ 3/2/dags/laboratory_2.py
@@ -1,26 +1,70 @@
-import datetime
-
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+from airflow.providers.apache.cassandra.hooks.cassandra import CassandraHook
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
 
 
-def do():
-    print('laboratory')
+def read_from_cassandra():
+    hook = CassandraHook(cassandra_conn_id='cassandra_default')
+    session = hook.get_conn()  # FIXME
+    session.set_keyspace('laboratory_1')
+    rows = session.execute("SELECT uuid FROM orders")
+    return [{"id": row.id, "value": row.value} for row in rows]
 
+
+def transform_data(**context):
+    data = context['ti'].xcom_pull(task_ids='read_data')
+    transformed = [{"id": str(row['id']), "updated_value": row['value'].upper()} for row in data]
+    return transformed
+
+
+def write_to_cassandra(**context):
+    transformed_data = context['ti'].xcom_pull(task_ids='transform_data')
+    # hook = CassandraHook(cassandra_conn_id='cassandra_default')
+    # session = hook.get_conn()
+
+    # for row in transformed_data:
+    #     session.execute(
+    #         """
+    #         INSERT INTO target_table (id, updated_value) VALUES (%s, %s)
+    #         """,
+    #         (uuid.UUID(row['id']), row['updated_value'])
+    #     )
+    pass
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
 
 with DAG(
-        dag_id="laboratory_2",
+        'fulfilling_orders',
+        default_args=default_args,
+        description='Laboratory 2. Fulfilling orders',
         schedule_interval=None,
-        tags=['laboratory_2'],
+        start_date=datetime(2023, 1, 1),
         catchup=False,
-):
-    # TODO: Доделать
-    laboratory_2_1 = PythonOperator(
-        task_id='laboratory_2_1',
-        python_callable=do,
+        tags=['example'],
+) as dag:
+    read_data = PythonOperator(
+        task_id='read_data',
+        python_callable=read_from_cassandra,
     )
 
-    laboratory_2_2 = EmptyOperator(task_id="laboratory_2_2")
+    transform_data = PythonOperator(
+        task_id='transform_data',
+        python_callable=transform_data,
+        provide_context=True,
+    )
 
-    laboratory_2_1 >> laboratory_2_2
+    write_data = PythonOperator(
+        task_id='write_data',
+        python_callable=write_to_cassandra,
+        provide_context=True,
+    )
+
+    read_data >> transform_data >> write_data
